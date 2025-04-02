def230ec3a209b10800bd4ed4d3ceab780aa5e83
Gaddameedi Naveena <gaddameedinaveena06@gmail.com>
2025-04-02 15:02:09 +0530

    uploading backend java files

---
+++ MyFirstProgram.java
@@ -0,0 +1,29 @@
+class MyFirstPogram{
+
+public static void main(String[] args){
+
+// printing statements
+ System.out.println("hi mam");
+}
+
+
+}
+
+
+
+
+/* creating a file with .java extension
+   eg:progrom1.java
+write basic code in file
+   
+  class program1.java{
+      public static void main(String[] args){
+   }
+}
+
+ complie the code cmd --> javav filename.java
+                          ex: javac program1.java
+ run the code 
+                       -->java classame.java
+                         ex:java program1.java
+*/
---
+++ Program2.java
@@ -0,0 +1,46 @@
+class Program2{
+
+public static void main(String []args){
+
+
+
+String name="Naveena";
+int age=21;
+long phone=26889998;
+String email="naveena@mail.com";
+System.out.println("Name: "+ name);
+System.out.println("Age:"+ age);
+System.out.println("Phone :"+ phone);
+System.out.println("Email:" +email);
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+/*
+ variable:
+        it acts like a container where it can store data
+        it is used to store data
+ Data type : it defines type of a data
+          1.primitive data type
+            byte-146
+            short
+            int
+            long
+            float
+            double
+            char -- 
+            boolean-- false/true
+*/
+
+
+
+
