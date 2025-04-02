fbe6bfe49b0f96c97245edf2ef7cf01a99ae6278
Gaddameedi Naveena <gaddameedinaveena06@gmail.com>
2025-04-02 19:59:38 +0530

    uploading backend files

---
+++ Program4.java
@@ -0,0 +1,47 @@
+class Program4{
+  
+ public static void main(String args[]){
+
+ 
+  
+    System.out.println(06);
+    System.out.println('n');
+    System.out.println("java");
+    System.out.println(true);
+ System.out.println("==============================");
+   String container1="am learning java";
+   System.out.println(container1);
+   System.out.println(container1);
+   System.out.println(container1);
+   System.out.println(container1);
+   System.out.println(container1);
+   System.out.println(container1);
+   System.out.println(container1); 
+   System.out.println("-------------------------------");
+   String value1="hi buddies";
+   int value2=10;
+   double value3=345.00;
+   char value4='x';
+   boolean value5=false;
+    System.out.println(value1);
+   System.out.println(value2);
+   System.out.println(value3);
+   System.out.println(value4);
+   System.out.println(value5);
+  System.out.println("+++++++++++++++++++++++++++++++++");
+   int a=10;
+  int b=10;
+String c="java";
+ System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+System.out.println(a+c+b);// 10java10
+System.out.println(c+b+a); //java1010
+System.out.println(a+b+c); //20java
+
+
+   
+}
+}
